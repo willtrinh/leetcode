073ad588e703d20d9a9aedb5429b08a7176d734f
Will Trinh <williamltrinh@gmail.com>
2021-06-03 11:18:57 -0700

    Runtime: 80 ms Memory Usage: 47 MB
    Solution using object for faster lookup (hash table)

--- contains-duplicate/contains-duplicate.js
+++ contains-duplicate/contains-duplicate.js
@@ -3,10 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const set = new Set();
+    const obj = {}; // keep track of occurrences for each num in nums
     for (const num of nums) {
-        if (set.has(num)) return true;
-        else set.add(num);
+        obj[num] = obj[num] + 1 || 1; // count occurrences
+        // if num appears at least twice then return true
+        if (obj[num] > 1) return true;
     }
+    // else return false if every element is distinct
     return false;
 };
