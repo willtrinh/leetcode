34d767329036095070c5f099e1f19e30f55f2698
Will Trinh <williamltrinh@gmail.com>
2021-06-22 19:27:41 -0700

    Update find-minimum-in-rotated-sorted-array.js

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -8,12 +8,29 @@
 // C: O(logn) time complexity
 // E: none
 
-// check if the next value is less than the current value
-// that would be the minimum value since the array is sorted
+// use a "modified" binary search
+// check if first value > last value in the array.
+// since the array is sorted, if the first value > last value
+// then that means the array has been rotated
+
 var findMin = function(nums) {
-    let minVal = nums[0];
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] < minVal) return nums[i];
+    if (nums.length === 1) return nums[0];
+    // if first value < last value, array is sorted and smallest value is first value 
+    let left = 0, right = nums.length - 1;
+    if (nums[0] < nums[right]) return nums[0];
+    
+    // binary search
+    while (right >= left) {
+        let mid = Math.floor(left + (right - left) / 2);
+        // if mid element is larger than its next element then mid+1 element is the smallest
+        if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
+        // if mid element is lesser than its prev element then mid element is the smallest
+        if (nums[mid] < nums[mid - 1]) return nums[mid];
+        // if mid element is larger than 0th element then we need to check right side
+        if (nums[mid] > nums[0]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-    return minVal;
 };
