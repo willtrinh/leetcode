2e76017927564b87c14ff1fc5eadd7d26422779f
Will Trinh <williamltrinh@gmail.com>
2021-08-01 17:47:48 -0700

    Time: 152 ms (60.73%), Space: 49.3 MB (64.82%) - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -2,51 +2,36 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-
-// I: integer array
-// O: all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0
-// C: solution set must not contain duplicate triplets
-// E: none
-
 var threeSum = function(nums) {
-    // if we don't have at least 3 numbers then return empty array
+    // if the input array doesn't have at least 3 numbers then there is no triplet
     if (nums.length < 3) return [];
-    
     let triplets = [];
     // sort the array
-    nums = nums.sort((a, b) => a - b);
+    nums.sort((a, b) => a - b);
     
     for (let i = 0; i < nums.length - 2; i++) {
-        // since the array is sorted, positive number can't sum to triplet's target sum
-        // which is 0, so no need to continue further
-        if (nums[i] > 0) break;
-        
-        // solution set cannot contain duplicate triplets so we need to skip duplicates
+        // skip duplicates
         if (i > 0 && nums[i] === nums[i - 1]) continue;
-        
-        let j = i + 1; // j is the middle index between i and k
-        let k = nums.length - 1; // k is the right most index
-        
-        // move j and k toward each other and increment i when they meet
+        let j = i + 1;
+        let k = nums.length - 1;
         while (j < k) {
             let sum = nums[i] + nums[j] + nums[k];
-            // push the triplet to triplets array if they add up to 0
             if (sum === 0) {
                 triplets.push([nums[i], nums[j], nums[k]]);
                 // increment j and decrement k to skip duplicates
-                while (nums[j] === nums[j + 1]) j++;
-                while (nums[k] === nums[k - 1]) k--;
-
+                while (nums[j] === nums[j+1]) j++;
+                while (nums[k] === nums[k-1]) k--;
                 // move j forward and k backward
                 j++;
                 k--;
-            // if the sum is smaller than 0, move j forward
+            // else if the sum is less than 0, move j forward
             } else if (sum < 0) {
                 j++;
-            // if sum is larger than 0, move k backward
+            // else if sum is larger than 0, move k backward    
             } else {
                 k--;
             }
+            
         }
     }
     
